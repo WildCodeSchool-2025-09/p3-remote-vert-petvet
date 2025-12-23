b74a964f147e33974309149aff2a59cbd255a144
LUNA SOLER <soler.luna@outlook.fr>
2025-12-23 17:08:18 +0100

    modificate the petseeder

--- server/database/fixtures/PetSeeder.ts
+++ server/database/fixtures/PetSeeder.ts
@@ -11,7 +11,7 @@ class PetSeeder extends AbstractSeeder {
         name: this.faker.animal.petName(),
         born_at: this.faker.date.birthdate(),
         species: this.faker.animal.type(),
-        breed: this.faker.animal.type(),
+        breed: this.faker.animal.dog(),
         owner_id: this.faker.number.int({ min: 1, max: 5 }),
         veterinary_id: this.faker.number.int({ min: 1, max: 3 }),
       };

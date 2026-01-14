92ea9ea3fe884c47af6b2469016e703f23a06c6c
Hugo Lobato <hugo.lobato110@gmail.com>
2026-01-14 11:53:14 +0100

    page, fetch, map done. Creation pages modules et set seeder

---
+++ client/src/pages/Reminders.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+interface Reminders {
+  id: number;
+  title: string;
+  programmed_at: number;
+  content: string;
+  dosage: number;
+  veterinary_id: number;
+  pet_id: number;
+  frequency: string;
+}
+
+function Reminders() {
+  const [reminders, setReminders] = useState<Reminders[]>([]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/reminder`)
+      .then((res) => res.json())
+      .then((reminders) => setReminders(reminders))
+      .catch(() => console.error("Erreur lors du chargement"));
+  });
+
+  return (
+    <div>
+        <h1>Mes rappels</h1>
+        {reminders.map((reminder) => (
+            <div key={reminder.id} className="reminder-card">
+                <h3>{reminder.title}</h3>
+                <p>Date: {reminder.programmed_at}</p>
+                {reminder.content && <p>{reminder.content}</p>}
+            </div>
+        ))}
+    </div>
+);
+
+}
+export default Reminders;
---
+++ server/database/fixtures/ReminderSeeder.ts
@@ -0,0 +1,35 @@
+import OwnerSeeder from "./OwnerSeeder";
+import VeterinarySeeder from "./VeterinarySeeder";
+import PetSeeder from "./PetSeeder";
+import AbstractSeeder from "./AbstractSeeder";
+
+class ReminderSeeder extends AbstractSeeder {
+  constructor() {
+    super({
+      table: "reminder",
+      truncate: true,
+      dependencies: [OwnerSeeder, PetSeeder, VeterinarySeeder],
+    });
+  }
+
+  run() {
+    for (let i = 0; i < 5; i++) {
+      const owner = this.getRef(`owner_${i}`);
+      const veterinary = this.getRef(`veterinary_${i}`);
+      const pet = this.getRef(`pet_${i}`);
+      const fakeReminder = {
+        title: this.faker.lorem.word(),
+        programmed_at: this.faker.date.anytime(),
+        content: this.faker.lorem.sentence(),
+        dosage: this.faker.helpers.arrayElement(['5mg', '10mg', '2ml']),
+        owner_id: owner.insertId,
+        veterinary_id: veterinary.insertId,
+        pet_id: pet.insertId
+      };
+
+      this.insert(fakeReminder);
+    }
+  }
+}
+
+export default ReminderSeeder;

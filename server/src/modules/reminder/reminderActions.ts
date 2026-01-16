9bb5d97f53ef9c2251e0159b49e4a1948fab07dc
leabpinheiro <leamelanie1998@hotmail.fr>
2026-01-16 10:10:37 +0100

    correction for error request

--- server/src/modules/reminder/reminderActions.ts
+++ server/src/modules/reminder/reminderActions.ts
@@ -1,3 +1,4 @@
+import { error } from "node:console";
 import type { RequestHandler } from "express";
 import ReminderRepository from "./reminderRepository";
 
@@ -7,7 +8,9 @@ const browseByPet: RequestHandler = async (req, res, next) => {
     const reminders = await ReminderRepository.findAllByPetId(petId);
 
     if (!reminders || reminders.length === 0) {
-      res.sendStatus(204);
+      res.status(404).json({
+        message: "Nous n'avons pas trouvé de rappels pour cet animal.",
+      });
     } else {
       res.status(200).json(reminders);
     }

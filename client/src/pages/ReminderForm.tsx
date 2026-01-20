081e84257adcc40d3ed25f7d9f14829725015021
LUNA SOLER <soler.luna@outlook.fr>
2026-01-20 11:08:13 +0100

    started the css but got issues with my pc

--- client/src/pages/ReminderForm.tsx
+++ client/src/pages/ReminderForm.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import "../assets/styles/reset.css";
+import "../assets/styles/variables.css";
+import "../assets/styles/reminderForm.css";
 
 type Frequency = "jour" | "semaine" | "mois" | "an";
 
@@ -73,74 +76,81 @@ function ReminderForm() {
   };
 
   return (
-    <>
-      <h1>Ajouter un rappel</h1>
-      <p>* : Champs obligatoires</p>
+    <body>
+      <header className="pet-vet">Pet&Vet</header>
       <section>
-        <p>{errorMessage}</p>
-        <p>{successMessage}</p>
-        <form onSubmit={submitReminder}>
-          <label>
-            Titre*
-            <input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Date programmée*
-            <input
-              type="datetime-local"
-              value={programmedAt}
-              onChange={(e) => setProgrammedAt(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Description*
-            <input
-              type="text"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Dosage
-            <input
-              type="text"
-              value={dosage}
-              onChange={(e) => setDosage(e.target.value)}
-            />
-          </label>
-          <label>
-            Fréquence
-            <input
-              type="number"
-              min={1}
-              value={frequencyCount}
-              placeholder="Nombre de fois"
-              onChange={(e) => setFrequencyValue(Number(e.target.value))}
-            />
-            <p>foir par</p>
-            <select
-              value={frequency}
-              onChange={(e) => setFrequency(e.target.value as Frequency)}
-            >
-              <option value="jour">jour</option>
-              <option value="semaine">semaine</option>
-              <option value="mois">mois</option>
-              <option value="an">an</option>
-            </select>
-          </label>
-          <button type="submit" disabled={isSubmitting}>
-            {isSubmitting ? "Création..." : "Créer un rappel"}
-          </button>
-        </form>
+        <h1 className="reminder-form-title">Ajouter un rappel</h1>
+        <p>* : Champs obligatoires</p>
+        <article className="form-container">
+          <p>{errorMessage}</p>
+          <p>{successMessage}</p>
+          <form onSubmit={submitReminder}>
+            <div className="title-date">
+              <label>
+                Titre*
+                <input
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  required
+                />
+              </label>
+              <label>
+                Date programmée*
+                <input
+                  type="datetime-local"
+                  value={programmedAt}
+                  onChange={(e) => setProgrammedAt(e.target.value)}
+                  required
+                />
+              </label>
+            </div>
+            <label>
+              Description*
+              <input
+                type="text"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Dosage
+              <input
+                type="text"
+                value={dosage}
+                onChange={(e) => setDosage(e.target.value)}
+              />
+            </label>
+            <div className="frequency-container">
+              <label>
+                Fréquence
+                <input
+                  type="number"
+                  min={1}
+                  value={frequencyCount}
+                  placeholder="Nombre de fois"
+                  onChange={(e) => setFrequencyValue(Number(e.target.value))}
+                />
+                <p>fois par</p>
+                <select
+                  value={frequency}
+                  onChange={(e) => setFrequency(e.target.value as Frequency)}
+                >
+                  <option value="jour">jour</option>
+                  <option value="semaine">semaine</option>
+                  <option value="mois">mois</option>
+                  <option value="an">an</option>
+                </select>
+              </label>
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Création..." : "Créer un rappel"}
+            </button>
+          </form>
+        </article>
       </section>
-    </>
+    </body>
   );
 }
 

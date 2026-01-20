9ae17aab475eec0e7e8f28a43497a59b45ab72a2
jeremy gontier <jeremy.gontier@gmail.com>
2026-01-20 21:25:30 +0100

    ajout du css

--- client/src/pages/HealthRecord.tsx
+++ client/src/pages/HealthRecord.tsx
@@ -1,1 +1,25 @@
+import { useState } from "react";
 import Reminder from "../components/Reminder/Reminder";
+
+import "../assets/styles/variables.css";
+
+export default function HealthRecord() {
+  const [isReminderOpen, setIsReminderOpen] = useState<boolean>(false);
+  const reminderId = 2;
+
+  return (
+    <div>
+      <h1>Health Record</h1>
+
+      <button type="button" onClick={() => setIsReminderOpen(true)}>
+        Ouvrir le reminder
+      </button>
+
+      <Reminder
+        reminderId={reminderId}
+        open={isReminderOpen}
+        onClose={() => setIsReminderOpen(false)}
+      />
+    </div>
+  );
+}

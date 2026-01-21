7c6ef4c614811ad30cde2dd58a9e1720a24348d0
Hugo Lobato <hugo.lobato110@gmail.com>
2026-01-21 11:14:54 +0100

    petId rectifié pardon <3

--- server/src/modules/pet/petRepository.ts
+++ server/src/modules/pet/petRepository.ts
@@ -2,13 +2,13 @@ import type { Rows } from "../../../database/client";
 import databaseClient from "../../../database/client";
 
 class petRepository {
-  async getByPet(id: number): Promise<Rows[0]> {
+  async getByPet(petId: number): Promise<Rows[0]> {
     const [pet] = await databaseClient.query<Rows>(
       `SELECT pet.*, veterinary.lastname
      FROM pet
      JOIN veterinary ON veterinary.id = pet.veterinary_id
      WHERE pet.id = ?`,
-      [id],
+      [petId],
     );
 
     return pet[0];

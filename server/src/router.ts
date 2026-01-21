15757fd3fd643ed213e4ded0a505e030f65e7be9
Hugo Lobato <hugo.lobato110@gmail.com>
2026-01-21 10:42:38 +0100

    fix naming

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,11 +1,10 @@
 import express from "express";
-//import PetActions from "./modules/pet/PetActions";
-//import * as ReminderActions from "./modules/reminder/ReminderActions";
+
 
 const router = express.Router();
 
-//router.get("/reminder/owner/:id", ReminderActions.browseByOwner);
+router.get("/reminder/owner/:id",);
 
-//router.get("/pet/:id", PetActions.read);
+router.get("/pet/:id", );
 
 export default router;

7744ec16197098bfc306ef3ff4d6c4c83335e3da
leabpinheiro <leamelanie1998@hotmail.fr>
2026-01-22 10:59:50 +0100

    update router and pet.ts

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,13 +1,7 @@
 import express from "express";
-import petActions from "../src/modules/pet/petActions";
+import petActions from "./modules/pet/petActions";
 const router = express.Router();
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
 router.get("/pet/:id", petActions.read);
-// Define item-related routes
-
-/* ************************************************************************* */
 
 export default router;

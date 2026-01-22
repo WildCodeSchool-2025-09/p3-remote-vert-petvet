f1aef7c094e771870dab18b8d8768e1142832536
jeremy gontier <jeremy.gontier@gmail.com>
2026-01-22 15:27:36 +0100

    creation de la page ConsultationForm et creation du formulaire et insertion des fetch

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 // Import necessary modules from React and React Router
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router";
 
@@ -7,6 +7,7 @@ import { RouterProvider, createBrowserRouter } from "react-router";
 
 // Import the main app component
 import App from "./App";
+import ConsultForm from "./pages/ConsultForm";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
     path: "/", // The root path
     element: <App />, // Renders the App component for the home page
   },
+  {
+    path: "/consultation/add",
+    element: <ConsultForm />,
+  },
   // Try adding a new route! For example, "/about" with an About component
 ]);
 
@@ -35,11 +40,7 @@ if (rootElement == null) {
 }
 
 // Render the app inside the root element
-createRoot(rootElement).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-);
+createRoot(rootElement).render(<RouterProvider router={router} />);
 
 /**
  * Helpful Notes:
---
+++ client/src/pages/ConsultForm.tsx
@@ -0,0 +1,217 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import "../assets/styles/reset.css";
+import "../assets/styles/variables.css";
+import "../assets/styles/consultForm.css";
+
+type Category = "vaccination" | "urgence" | "suivi" | "opération" | "médicale";
+
+interface CreateConsult {
+  title: string;
+  programmedAt: string;
+  report: string;
+  dosage: string | null;
+  category: Category | null;
+  treatment: string | null;
+}
+
+type Animal = {
+  id: number;
+  name: string;
+};
+
+function ConsultForm() {
+  const [title, setTitle] = useState("");
+  const [programmedAt, setProgrammedAt] = useState("");
+  const [report, setReport] = useState("");
+  const [dosage, setDosage] = useState("");
+  const [category, setCategory] = useState<Category | "">("");
+  const [treatment, setTreatment] = useState<string | "">("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [successMessage, setSuccessMessage] = useState<string>("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [selectedAnimal, setSelectedAnimal] = useState<string>("");
+  const [animals, setAnimals] = useState<Animal[]>([]);
+  const { vetId } = useParams<{ vetId: string }>();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!vetId) return;
+
+    fetch(`${import.meta.env.VITE_API_URL}/consult/${vetId}`)
+      .then((res) => res.json())
+      .then((data) => setAnimals(data))
+      .catch((err) => console.error(err));
+  }, [vetId]);
+
+  const createConsult = async (Consult: CreateConsult) => {
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/consult`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Consult),
+      });
+
+      if (!response.ok) {
+        throw new Error("Erreur lors de la création de la consultation");
+      }
+
+      setSuccessMessage("Consultation créé avec succès !");
+
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    } catch (error: unknown) {
+      setErrorMessage(
+        error instanceof Error
+          ? error.message
+          : "Erreur lors de la création de la consultation",
+      );
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const submitConsult = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const newConsult = {
+      title: title,
+      programmedAt: programmedAt,
+      report: report,
+      dosage: dosage || null,
+      category: category || null,
+      treatment: treatment || null,
+    };
+
+    createConsult(newConsult);
+  };
+
+  return (
+    <section>
+      <header className="pet-vet">Pet&Vet</header>
+      <h1 className="Consult-form-title">Ajouter une consultation</h1>
+      <article className="form-container">
+        <form onSubmit={submitConsult}>
+          <p className="error">{errorMessage}</p>
+          <p className="success">{successMessage}</p>
+          <div className="category-value">
+            <select
+              value={category}
+              aria-placeholder="category"
+              onChange={(e) => setCategory(e.target.value as Category)}
+            >
+              <option value="" disabled hidden>
+                Select a category
+              </option>
+              <option value="vaccination">vaccination</option>
+              <option value="urgence">urgence</option>
+              <option value="suivi">suivi</option>
+              <option value="opération">opération</option>
+              <option value="médicale">médicale</option>
+            </select>
+          </div>
+          <div className="date">
+            <label>
+              Date programmée <span className="obligatory">*</span>
+              <input
+                type="datetime-local"
+                value={programmedAt}
+                onChange={(e) => setProgrammedAt(e.target.value)}
+                required
+                className="date"
+              />
+            </label>
+          </div>
+          <div className="animal_name">
+            <select
+              value={selectedAnimal}
+              aria-placeholder="animal"
+              onChange={(e) => setSelectedAnimal(e.target.value)}
+            >
+              <option value="" disabled hidden>
+                Sélectionne un animal
+              </option>
+              {animals.map((animals) => (
+                <option key={animals.id} value={animals.name}>
+                  {animals.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="title">
+            <label>
+              Titre <span className="obligatory">*</span>
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+                className="title_input"
+              />
+            </label>
+          </div>
+          <div className="content">
+            <label>
+              <textarea
+                placeholder="Détails de la consultation : "
+                value={report}
+                onChange={(e) => {
+                  const value = e.target.value;
+                  if (!value.startsWith("Détails de la consultation : ")) {
+                    setReport("Détails de la consultation : ");
+                  } else {
+                    setReport(value);
+                  }
+                }}
+                required
+                className="content"
+              />
+            </label>
+          </div>
+          <div className="treatment">
+            <label>
+              Traitement
+              <input
+                type="text"
+                value={treatment}
+                placeholder="traitement"
+                onChange={(e) => setTreatment(String(e.target.value))}
+                className="treatment_input"
+              />
+            </label>
+          </div>
+          <div className="dosage">
+            <label>
+              Posologie
+              <input
+                type="text"
+                value={dosage}
+                placeholder="posologie"
+                onChange={(e) => setDosage(e.target.value)}
+                className="dosage_input"
+              />
+            </label>
+          </div>
+          <div className="button-container">
+            <p className="obligatory">* Champs obligatoires</p>
+            <button
+              type="submit"
+              className="send_button"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Création..." : "Créer une consultation"}
+            </button>
+          </div>
+        </form>
+      </article>
+    </section>
+  );
+}
+
+export default ConsultForm;

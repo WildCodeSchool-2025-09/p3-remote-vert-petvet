a5ed3893bd823d031086ac7443e2ffb0da1c736e
LUNA SOLER <soler.luna@outlook.fr>
2026-01-30 10:58:27 +0100

    ça me saoule

--- server/src/modules/reminder/reminderRepository.ts
+++ server/src/modules/reminder/reminderRepository.ts
@@ -18,7 +18,11 @@ export interface Reminder {
 class reminderRepository {
   async getByPet(petId: number) {
     const [petReminders] = await databaseClient.query(
-      "SELECT reminder.*, pet.name FROM reminder JOIN pet ON reminder.pet_id = pet.id WHERE reminder.pet_id = ? ORDER BY reminder.programmed_at ASC",
+      `SELECT reminder.*, pet.name 
+    	FROM reminder 
+    	JOIN pet ON reminder.pet_id = pet.id 
+    	WHERE reminder.pet_id = ? 
+     	ORDER BY reminder.programmed_at ASC`,
       [petId],
     );
 
@@ -27,7 +31,7 @@ class reminderRepository {
 
   async getByOwner(ownerId: number): Promise<Rows> {
     const [reminders] = await databaseClient.query<Rows>(
-      "SELECT reminder.*, pet.name, pet.photo FROM reminder JOIN pet ON pet.id = reminder.pet_id WHERE reminder.owner_id = ?",
+      "SELECT reminder.*, pet.photo, pet.name FROM reminder JOIN pet ON pet.id = reminder.pet_id WHERE reminder.owner_id = ?",
       [ownerId],
     );
     return reminders;

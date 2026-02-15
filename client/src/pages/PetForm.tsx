414c682c8152b07ee1029d195c78262137bda3a5
Hugo Lobato <hugo.lobato110@gmail.com>
2026-02-15 16:08:36 +0100

    formulaire fini et CSS desktop done

--- client/src/pages/PetForm.tsx
+++ client/src/pages/PetForm.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 import styles from "../assets/styles/petForm.module.css";
 import type { CreatePet, Gender, Specie } from "../types/Pet";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
 
 function PetForm() {
   const [name, setName] = useState("");
@@ -54,162 +56,180 @@ function PetForm() {
   const today = new Date().toISOString().split("T")[0];
 
   return (
-    <section className={styles.petFormPage}>
-      <nav className={styles.navPetForm}>
+    <>
+      <header className={styles.petVet}>
         <img
           src={"/images/green/logo.png"}
           alt="logo-petvet"
           className={styles.logoPet}
         />
-        <header className={styles.petVet}>Pet&Vet</header>
-      </nav>
-      <h1 className={styles.petFormTitle}>Ajouter un animal</h1>
-      <article className={styles.petFormContainer}>
-        <form
-          className={styles.petForm}
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (!gender || !specie) {
-              setErrorMessage("Veuillez sélectionner le sexe et l'espèce");
-              return;
-            }
-            createPet({
-              name,
-              tattoo_nb: tattooNb || null,
-              chip_nb: chipNb ? Number(chipNb) : null,
-              born_at: bornAt,
-              gender: gender as Gender,
-              specie: specie as Specie,
-              breed,
-              is_neutered: isNeutered,
-              weight: weight ? Number(weight) : null,
-            });
-          }}
-        >
-          <p className={styles.error}>{errorMessage}</p>
-          <label className={styles.petLabel}>
-            Nom <span className={styles.obligatory}>*</span>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-              className={styles.petNameInput}
-            />
-          </label>
-
-          <label className={styles.petLabel}>
-            Numéro de tatouage
-            <input
-              type="number"
-              value={tattooNb}
-              onChange={(e) => setTattooNb(e.target.value)}
-              className={styles.tattooPetInput}
-            />
-          </label>
-
-          <label className={styles.chipNbPetInput}>
-            Numéro de puce
-            <input
-              type="number"
-              value={chipNb}
-              onChange={(e) => setChipNb(e.target.value)}
-              className={styles.chipNbPetInput}
-            />
-          </label>
-
-          <label className={styles.bornAtLabel}>
-            Date de naissance <span className={styles.obligatory}>*</span>
-            <input
-              type="date"
-              name="Date-de-naissance"
-              max={today}
-              value={bornAt}
-              onChange={(e) => setBornAt(e.target.value)}
-              required
-              className={styles.bornAtPetInput}
-            />
-          </label>
-
-          <label className={styles.genderLabel}>
-            Sexe <span className={styles.obligatory}>*</span>
-            <select
-              className="sexeValue"
-              value={gender}
-              onChange={(e) => setGender(e.target.value as Gender)}
-            >
-              <option value="" disabled>
-                Sélectionnez le sexe
-              </option>
-              <option value="f">Femelle</option>
-              <option value="m">Mâle</option>
-            </select>
-          </label>
-
-          <label className={styles.SpecieLabel}>
-            Espece<span className={styles.obligatory}>*</span>
-            <select
-              value={specie}
-              onChange={(e) => setSpecie(e.target.value as Specie)}
-            >
-              <option value="" disabled>
-                Sélectionnez l'espèce de votre animal
-              </option>
-
-              <option value="Chien">Chien</option>
-
-              <option value="Chat">Chat</option>
-
-              <option value="Lapin">Lapin</option>
-            </select>
-          </label>
-
-          <label className={styles.breedLabel}>
-            Race<span className={styles.obligatory}>*</span>
-            <input
-              type="text"
-              value={breed}
-              onChange={(e) => setBreed(e.target.value)}
-              required
-              className={styles.breedPetInput}
-            />
-          </label>
-
-          <div className={styles.toggleNeutered}>
-            <input
-              type="checkbox"
-              id="modeSwitchNeutered"
-              className={styles.checkbox}
-              checked={isNeutered}
-              onChange={() => setIsNeutered(!isNeutered)}
-            />
-            <label htmlFor="modeSwitchNeutered" className={styles.toggleLabel}>
-              <span
-                className={`${styles.labelText} ${!isNeutered ? styles.active : ""}`}
+        <h1>Pet&Vet</h1>
+      </header>
+      <main className={styles.petFormPage}>
+        <div className={styles.mainPage}>
+          <NavBar />
+          <div className={styles.petFormContainer}>
+            <h1 className={styles.petFormTitle}>Ajouter un animal</h1>
+            <article className={styles.background}>
+              <form
+                className={styles.petForm}
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  if (!gender || !specie) {
+                    setErrorMessage(
+                      "Veuillez sélectionner le sexe et l'espèce",
+                    );
+                    return;
+                  }
+                  createPet({
+                    name,
+                    tattoo_nb: tattooNb || null,
+                    chip_nb: chipNb ? Number(chipNb) : null,
+                    born_at: bornAt,
+                    gender: gender as Gender,
+                    specie: specie as Specie,
+                    breed,
+                    is_neutered: isNeutered,
+                    weight: weight ? Number(weight) : null,
+                  });
+                }}
               >
-                Non
-              </span>
-              <span
-                className={`${styles.labelText} ${isNeutered ? styles.active : ""}`}
-              >
-                Oui
-              </span>
-              <div className={styles.switchSlider} />
-            </label>
-          </div>
+                <p className={styles.error}>{errorMessage}</p>
+                <label className={styles.petLabelName}>
+                  {"Nom"}
+                  <span className={styles.obligatory}>* :</span>
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                    placeholder="Nom"
+                    className={styles.petNameInput}
+                  />
+                </label>
+                <label className={styles.petLabelTattoo}>
+                  {"Numéro de tatouage :"}
+                  <input
+                    type="number"
+                    value={tattooNb}
+                    onChange={(e) => setTattooNb(e.target.value)}
+                    placeholder="Numéro de tatouage"
+                    className={styles.tattooPetInput}
+                  />
+                </label>
 
-          <label className={styles.weightLabel}>
-            Poids<span className={styles.obligatory}>*</span>
-            <input
-              type="number"
-              value={weight}
-              onChange={(e) => setWeight(e.target.value)}
-              required
-              className={styles.weightPetInput}
-            />
-          </label>
+                <label className={styles.petLabelChip}>
+                  {"Numéro de puce :"}
+                  <input
+                    type="number"
+                    value={chipNb}
+                    onChange={(e) => setChipNb(e.target.value)}
+                    placeholder="Numéro de puce"
+                    className={styles.chipNbPetInput}
+                  />
+                </label>
 
-          {/* <label className={styles.photoLabel}>
+                <label className={styles.petLabelBorn}>
+                  {"Date de naissance"}
+                  <span className={styles.obligatory}>* :</span>
+                  <input
+                    type="date"
+                    name="Date-de-naissance"
+                    max={today}
+                    value={bornAt}
+                    onChange={(e) => setBornAt(e.target.value)}
+                    required
+                    className={styles.bornAtPetInput}
+                  />
+                </label>
+
+                <label className={styles.petLabelGender}>
+                  {"Sexe"}
+                  <span className={styles.obligatory}>* :</span>
+                  <select
+                    className="sexeValue"
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value as Gender)}
+                  >
+                    <option value="" disabled>
+                      Sélectionnez le sexe :
+                    </option>
+                    <option value="f">Femelle</option>
+                    <option value="m">Mâle</option>
+                  </select>
+                </label>
+
+                <label className={styles.petLabelSpecie}>
+                  {"Espece"}
+                  <span className={styles.obligatory}>* :</span>
+                  <select
+                    value={specie}
+                    onChange={(e) => setSpecie(e.target.value as Specie)}
+                  >
+                    <option value="" disabled>
+                      Sélectionnez l'espèce de votre animal
+                    </option>
+
+                    <option value="Chien">Chien</option>
+
+                    <option value="Chat">Chat</option>
+
+                    <option value="Lapin">Lapin</option>
+                  </select>
+                </label>
+
+                <label className={styles.petLabelBreed}>
+                  {"Race"}
+                  <input
+                    type="text"
+                    value={breed}
+                    onChange={(e) => setBreed(e.target.value)}
+                    required
+                    placeholder="Race"
+                    className={styles.breedPetInput}
+                  />
+                </label>
+
+                <div className={styles.toggleNeutered}>
+                  <input
+                    type="checkbox"
+                    id="modeSwitchNeutered"
+                    className={styles.checkbox}
+                    checked={isNeutered}
+                    onChange={() => setIsNeutered(!isNeutered)}
+                  />
+                  Stérilisé :
+                  <label
+                    htmlFor="modeSwitchNeutered"
+                    className={styles.toggleLabel}
+                  >
+                    <span
+                      className={`${styles.labelText} ${!isNeutered ? styles.active : " "}`}
+                    >
+                      Non
+                    </span>
+                    <span
+                      className={`${styles.labelText} ${isNeutered ? styles.active : " "}`}
+                    >
+                      Oui
+                    </span>
+                    <div className={styles.switchSlider} />
+                  </label>
+                </div>
+
+                <label className={styles.petLabelWeight}>
+                  <input
+                    type="number"
+                    value={weight}
+                    onChange={(e) => setWeight(e.target.value)}
+                    required
+                    placeholder="Poids"
+                    className={styles.weightPetInput}
+                  />
+                  <span>Kg</span>
+                </label>
+
+                {/* <label className={styles.photoLabel}>
               Photo
               <span className={styles.obligatory}>*</span>
               <input
@@ -224,13 +244,20 @@ function PetForm() {
             <div className={styles.FormPhotoContainer}>
               <img src={photo} alt={name} />
             </div> */}
-
-          <button type="submit" disabled={isSubmited}>
-            {isSubmited ? "Création..." : "Enregistrer"}
-          </button>
-        </form>
-      </article>
-    </section>
+                <div className={styles.petLabelSubmit}>
+                  <button type="submit" disabled={isSubmited}>
+                    {isSubmited ? "Création..." : "Enregistrer"}
+                  </button>
+                </div>
+              </form>
+            </article>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 }
 
